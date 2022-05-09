c0ec90f4c76e6789d464668be3dedbe794f34933
abahha77 <86109883+abahha77@users.noreply.github.com>
2022-05-09 14:38:32 -0700

    Add files via upload

---
+++ chatroom_info.cpp
@@ -0,0 +1,21 @@
+#include"chatroom_info.h"
+void chatroom_info::set_user_id(int u_id)
+{
+	id = u_id;
+}
+int chatroom_info::get_user_id()
+{
+	return id;
+}
+void chatroom_info::set_chatroom_id(int ch_id)
+{
+	chat_id= ch_id;
+}
+int chatroom_info::get_chatroom_id()
+{
+	return chat_id;
+}
+void chatroom_info::lastseen()
+{
+
+}
